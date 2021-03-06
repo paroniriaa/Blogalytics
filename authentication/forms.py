4ee7357d9adbb8cd9b7bc45e9f55a3f971f88729
paroniriaa <44891450+paroniriaa@users.noreply.github.com>
2021-03-05 22:49:13 -0800

    minor change
    minor change

--- authentication/forms.py
+++ authentication/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django import forms
 
 class SignupForm(UserCreationForm):
+    username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'type': "text", 'placeholder': "Username", 'name': "username", 'maxlength': "100",  'id': "id_username"}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
     first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
     last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
@@ -18,6 +19,7 @@ class SignupForm(UserCreationForm):
         self.fields['password2'].widget.attrs['class'] = 'form-control'
 
 class EditProfileForm(UserChangeForm):
+    username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'type': "text", 'placeholder': "Username", 'name': "username", 'maxlength': "100",  'id': "id_username"}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'type': "email", 'placeholder': "Email", 'name': "email", 'maxlength': "100", 'required': True, 'id': "id_email"}))
     first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'type': "text", 'placeholder': "First Name", 'name': "firstname", 'maxlength': "100",  'id': "id_first_name"}))
     last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'type': "text", 'placeholder': "Last Name", 'name': "lastname", 'maxlength': "100",  'id': "id_last_name"}))
