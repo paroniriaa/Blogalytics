b42b3b57b8c8bf6e2acea98cad12f2642030e175
tttttliao <35547532+tttttliao@users.noreply.github.com>
2021-03-06 11:39:07 +0800

    Rollback from Postgre to sqlite
    Rollback from Postgre to sqlite

--- Blogalytics/settings.py
+++ Blogalytics/settings.py
@@ -76,14 +76,20 @@ WSGI_APPLICATION = 'Blogalytics.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'blogalytics',
+#         'USER': 'admin',
+#         'PASSWORD': 'admin',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'blogalytics',
-        'USER': 'admin',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
