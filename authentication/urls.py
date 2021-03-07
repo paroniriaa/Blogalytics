97008e972c9f0790fd20fecd1a39f0a6f98b074a
franklinnwren <45508028+franklinnwren@users.noreply.github.com>
2021-03-08 03:10:00 +0800

    Add files via upload

---
+++ authentication/urls.py
@@ -0,0 +1,10 @@
+from . import views
+from django.urls import path
+from .views import UserRegisterView, PasswordChangeView, UserLoginView
+# from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('register/', UserRegisterView.as_view(), name='register'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('password/', PasswordChangeView.as_view(template_name='registration/change-password.html'), name='password'),
+]
