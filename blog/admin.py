c69973cf9f1367a9ca6e3383cfe8ad08ad533585
franklinnwren <45508028+franklinnwren@users.noreply.github.com>
2021-03-08 03:47:35 +0800

    Add files via upload

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Profile
 
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'status', 'created_on')
-    list_filter = ("status",)
+    list_display = ('title', 'slug', 'created_on', 'author')
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
   
 admin.site.register(Post, PostAdmin)
+admin.site.register(Profile)
